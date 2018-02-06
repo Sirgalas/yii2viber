9ec84aee829af66939cbf2748c08cb248eef4363
mike <kev@bernik.ru>
2018-02-06 08:06:44 +0700

    viber message new fields

--- common/entities/ViberMessage.php
+++ common/entities/ViberMessage.php
@@ -2,8 +2,8 @@
 
 namespace common\entities;
 
-use Yii;
 
+use common\entities\user\User;
 /**
  * This is the model class for table "viber_message".
  *
@@ -20,8 +20,12 @@ use Yii;
  * @property int $date_finish
  * @property string $time_start
  * @property string $time_finish
+ * @property string $status
+ * @property int $limit_messages Сколько сообщений отправлять?
+ * @property string $cost Стоимость
+ * @property string $balance Сколько средств уже потрачено
  *
- * @property User $user
+ * @property user/User $user
  */
 class ViberMessage extends \yii\db\ActiveRecord
 {
@@ -39,16 +43,25 @@ class ViberMessage extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['user_id', 'date_start', 'date_finish'], 'default', 'value' => null],
-            [['user_id', 'date_start', 'date_finish'], 'integer'],
+            [['user_id', 'date_start', 'date_finish', 'limit_messages'], 'default', 'value' => null],
+            [['user_id', 'date_start', 'date_finish', 'limit_messages'], 'integer'],
             [['title', 'text', 'image', 'title_button', 'url_button'], 'required'],
+            [['cost', 'balance'], 'number'],
             [['title'], 'string', 'max' => 50],
             [['text'], 'string', 'max' => 120],
             [['image', 'url_button'], 'string', 'max' => 255],
             [['title_button', 'alpha_name'], 'string', 'max' => 32],
             [['type'], 'string', 'max' => 10],
+            ['type', 'in', 'range' => array_keys(static::listTypes())],
             [['time_start', 'time_finish'], 'string', 'max' => 5],
-            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+            [['status'], 'string', 'max' => 16],
+            [
+                ['user_id'],
+                'exist',
+                'skipOnError' => true,
+                'targetClass' => User::className(),
+                'targetAttribute' => ['user_id' => 'id'],
+            ],
         ];
     }
 
@@ -71,6 +84,10 @@ class ViberMessage extends \yii\db\ActiveRecord
             'date_finish' => 'Date Finish',
             'time_start' => 'Time Start',
             'time_finish' => 'Time Finish',
+            'status' => 'Status',
+            'limit_messages' => 'Limit Messages',
+            'cost' => 'Cost',
+            'balance' => 'Balance',
         ];
     }
 
@@ -89,5 +106,34 @@ class ViberMessage extends \yii\db\ActiveRecord
     public static function find()
     {
         return new ViberMessageQuery(get_called_class());
+    }
+
+    public static function listTypes()
+    {
+        return [
+            'only_text' => 'Только текст (Официально)',
+            'only_image' => 'Только изображение (Официально)',
+            'text_and_button' => 'Текст + кнопка (Официально)',
+            'text_and_button_and_image' => 'Текст + кнопка + изображение (Официально)',
+        ];
+    }
+
+    public static function getTypeText($ind)
+    {
+        $list = static::listTypes();
+
+        return $list[$ind];
+    }
+
+    public static function getTypeComment($ind)
+    {
+        $list = [
+            'only_text' => 'абонент получает текст',
+            'only_image' => 'абонент получает картинку',
+            'text_and_button' => 'абонент получает текстовое сообщение, под которым расположена кнопка. При нажатии на кнопку – происходит переход по заданной ссылке',
+            'text_and_button_and_image' => 'абонент получает текстовое сообщение, под которым расположены картинка и кнопка',
+        ];
+
+        return $list[$ind];
     }
 }
