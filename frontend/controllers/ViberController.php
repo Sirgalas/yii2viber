c9453ce3fc75d94ed69a704c89f83800e46c4d15
mike <kev@bernik.ru>
2018-02-10 14:11:10 +0700

    viberController

---
+++ frontend/controllers/ViberController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace frontend\controllers;
+
+class ViberController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionReport()
+    {
+       file_put_contents('post' . time() .'.txt', print_r($_POST,1));
+       return 'OK';
+    }
+
+}
