2821485917cf8036881f7ddc1e9d7007307fb566
sirgalas <hudos@rambler.ru>
2018-02-21 00:13:35 +0300

    виджет

--- frontend/widgets/views/dealer.php
+++ frontend/widgets/views/dealer.php
@@ -2,10 +2,11 @@
 /**
  * @var $dealer common\entities\user\User
  */
+
 ?>
 <div class="col-md 10 col-md-offset-1 dealer_widget">
     <h4>Контакты Вашего дилера</h4>
-    <p><?=$dealer->first_name?$dealer->first_name:$dealer->username ?> <?= $dealer->surname?$dealer->surname:''; ?> <?=$dealer->family?$dealer->family:''; ?></p>
+    <p><?= ($dealer->first_name)?$dealer->first_name:$dealer->username ?> <?= $dealer->surname?$dealer->surname:''; ?> <?=$dealer->family?$dealer->family:''; ?></p>
     <p><?= $dealer->tel; ?></p>
     <p><?= $dealer->email ?></p>
     <p><?= $dealer->time_work; ?></p>
