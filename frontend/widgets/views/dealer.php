3effff949b45cf9450e79b2bd30c8b16c02e15db
sirgalas <hudos@rambler.ru>
2018-02-20 22:00:37 +0300

    переводы и профиль

--- frontend/widgets/views/dealer.php
+++ frontend/widgets/views/dealer.php
@@ -5,7 +5,8 @@
 ?>
 <div class="col-md 10 col-md-offset-1 dealer_widget">
     <h4>Контакты Вашего дилера</h4>
-    <p><?= $dealer->tel; ?> <?=$dealer->username ?></p>
+    <p><?=$dealer->first_name?$dealer->first_name:$dealer->username ?> <?= $dealer->surname?$dealer->surname:''; ?> <?=$dealer->family?$dealer->family:''; ?></p>
+    <p><?= $dealer->tel; ?></p>
     <p><?= $dealer->email ?></p>
     <p><?= $dealer->time_work; ?></p>
 </div>
