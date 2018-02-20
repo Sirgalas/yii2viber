9ece931e8da15c888590aeda9a05eed5153d2e32
sirgalas <hudos@rambler.ru>
2018-02-20 15:17:24 +0300

    редактирование phone

--- frontend/controllers/StatisticsController.php
+++ frontend/controllers/StatisticsController.php
@@ -49,6 +49,7 @@ class StatisticsController extends Controller
         if(preg_match('%(\d)+%',($post['titleSearch']))||is_array($post['status'])){
             $searchModel = new StatisticsMongoSearch();
         }
+        ViberTransaction::find()->where(['user_id'=>Yii::$app->user->identity->id]);
         $dataProvider = $searchModel->search(Yii::$app->request->post('ViberTransaction'));
         return $this->render('index', compact('model','contact_collections','searchModel', 'dataProvider','clients','post','messagePhoneList'));
     }
