32162653f87c400480e8c179083260a70e6b8682
mike <kev@bernik.ru>
2018-02-20 13:32:57 +0700

    change password from params

--- common/config/params.php
+++ common/config/params.php
@@ -1,18 +1,4 @@
 <?php
 return [
-    'adminEmail' => 'admin@vibershop24.ru',
-    'supportEmail' => 'admin@vibershop24.ru',
-    'user.passwordResetTokenExpire' => 3600,
-    'cookieDomain' => '.vibershop24.ru',
-    'frontendHostInfo' => 'http://vibershop24.ru',
-    'backendHostInfo' => 'http://vibershop24.ru/admin',
-    'cost'=>1,
-    'viber' => [
 
-        'login' => 'viber1804',
-        'secret' => 'NxQTivMZ',
-        'from' => 'Clickbonus',
-        'transaction_size_limit'=>2, // максимальное количество телефонов в одной транзакции
-        'min_delay' => 2, // Минимальное время между отправкой транзакций
-    ],
 ];
