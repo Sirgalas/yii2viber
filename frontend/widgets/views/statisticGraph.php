ca537668538e019aac7d13060a97f37fad033253
sirgalas <hudos@rambler.ru>
2018-02-22 22:30:38 +0300

    add formatter

---
+++ frontend/widgets/views/statisticGraph.php
@@ -0,0 +1,22 @@
+<?php
+use sirgalas\Morris;
+?>
+<div class="col-md-6">
+<?php
+if(!empty($dataArr)){
+    echo Morris\Donut::widget([
+        'resize' => true,
+        'element' => 'donutChart',
+        'data' => $dataArr,
+        'donutColor' => $background,
+        'formatter'  => "function (y) { return  y +'%' }"
+    ]);
+}
+?>
+</div>
+<div class="col-md-6">
+    <ul class="statusLi col-md-6">
+        <?= $datali ?>
+    </ul>
+
+</div>
