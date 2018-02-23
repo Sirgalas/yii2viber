506c68ae1ddbf33e043187c7e839eec6fc4f26c0
sirgalas <hudos@rambler.ru>
2018-02-23 22:40:42 +0300

    редактирование ошибок

--- common/mailers/WantDealer.php
+++ common/mailers/WantDealer.php
@@ -6,19 +6,18 @@ use common\entities\user\User;
 use Yii;
 class WantDealer
 {
-    public function send(User $user,  $dealer ): void
+    public function send(User $user,  $dealer )
     {
         if ($dealer){
             $to = $dealer->email;
         } else {
             $to = Yii::$app->params['supportEmail'];
         }
-        $sent=Yii::$app->mailer
-            ->compose('dealer/wantDealer.php', ['clients' => $user])
+        $sent=Yii::$app->mailer;
+        $sent->compose('dealer/wantDealer.php', ['clients' => $user])
             ->setFrom(\Yii::$app->params['adminEmail'])
             ->setTo($to)
             ->setSubject(Yii::t('mailer','You clients'.$user->username.' want dealer'))
             ->send();
-
     }
 }
