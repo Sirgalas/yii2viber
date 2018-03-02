96673e397af2631b67182d2394bcec9682bfe1a7
sirgalas <hudos@rambler.ru>
2018-03-02 15:59:48 +0300

    убрал ни кому не нужный поиск

--- frontend/views/layouts/left.php
+++ frontend/views/layouts/left.php
@@ -31,17 +31,7 @@ $action = Yii::$app->controller->action->id;
 
             </div>
         </div>
-
-        <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search..."/>
-                <span class="input-group-btn">
-                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-            </div>
-        </form>
+        
         <?php
         $menuItems = [
             ['label' => 'Меню приложения', 'options' => ['class' => 'header']],
