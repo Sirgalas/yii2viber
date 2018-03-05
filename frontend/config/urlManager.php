72d55ca1e4ff84f7b63716e9efb69b3712bec9bf
sirgalas <hudos@rambler.ru>
2018-03-05 10:26:37 +0300

    добавил регистрацию по токену

--- frontend/config/urlManager.php
+++ frontend/config/urlManager.php
@@ -7,11 +7,13 @@ return  [
     'showScriptName' => false,
     'rules' => [
         '' => 'site/index',
+        'auth/register/<id:\w+>' => 'user/registration/register',
         '<_a:login|logout>' => 'site/<_a>',
         '<_c:[\w\-]+>' => '<_c>/index',
         '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
         '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
-        '<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_c>/<_a>',
-        'tst/viber/notification'=>'tst/viber/notification'
+        '<_c:[\w\-]+>/<_a:[\w\-]+>/<id:\d+>' => '<_c>/<_a>',
+        'tst/viber/notification'=>'tst/viber/notification',
+
     ],
 ]; ?>
