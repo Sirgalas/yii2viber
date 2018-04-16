8b90fa6383fd5698385c1e0c1ce3b6f7d8bb9f58
mike <kev@bernik.ru>
2018-04-16 18:41:39 +0700

    fix

--- common/entities/ViberMessage.php
+++ common/entities/ViberMessage.php
@@ -341,10 +341,10 @@ class ViberMessage extends \yii\db\ActiveRecord
     {
         $oldCost = $this->cost;
         $new_cost = self::cost($this->$attribute);
-        if ($new_cost - $oldCost > Yii::$app->user->identity->balance) {
-            $this->addError($attribute, 'Недостаточно средств на балансе');
-            //echo 'Недостаточно средств на балансе';
-        }
+        //if ($new_cost - $oldCost > Yii::$app->user->identity->balance) {
+        //    $this->addError($attribute, 'Недостаточно средств на балансе');
+        //    echo 'Недостаточно средств на балансе';
+        //}
 
         if ($this->scenario == ViberMessage::SCENARIO_HARD && $new_cost < 1) {
             $this->addError($attribute, 'Нет телефонов в рассылке');
