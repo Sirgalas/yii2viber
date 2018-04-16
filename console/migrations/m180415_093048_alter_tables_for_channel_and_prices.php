b71e2d083e95a3025f6fd44312148272a5f0c564
mike <kev@bernik.ru>
2018-04-16 13:22:51 +0700

    migrate fix

--- console/migrations/m180415_093048_alter_tables_for_channel_and_prices.php
+++ console/migrations/m180415_093048_alter_tables_for_channel_and_prices.php
@@ -13,11 +13,11 @@ class m180415_093048_alter_tables_for_channel_and_prices extends Migration
     public function safeUp()
     {
         $this->addColumn('{{%balance}}', 'viber_price',$this->string(20));
-        $this->addColumn('{{%balance}}', 'whatsapp',$this->string(20));
+        //$this->addColumn('{{%balance}}', 'whatsapp',$this->string(20));
         $this->addColumn('{{%balance}}', 'whatsapp_price',$this->string(20));
         $this->addColumn('{{%balance}}', 'telegram_price',$this->string(20));
         $this->addColumn('{{%balance}}', 'wechat_price',$this->string(20));
-        $this->dropColumn('{{%balance}}', 'watsapp');
+        //$this->dropColumn('{{%balance}}', 'watsapp');
         $this->addColumn('{{%balance_log}}', 'channel',$this->string(20));
 
 
@@ -31,11 +31,11 @@ class m180415_093048_alter_tables_for_channel_and_prices extends Migration
     {
         $this->dropColumn('{{%balance_log}}', 'channel');
         $this->dropColumn('{{%balance}}', 'viber_price');
-        $this->dropColumn('{{%balance}}', 'whatsapp');
+        //$this->dropColumn('{{%balance}}', 'whatsapp');
         $this->dropColumn('{{%balance}}', 'whatsapp_price');
         $this->dropColumn('{{%balance}}', 'telegram_price');
         $this->dropColumn('{{%balance}}', 'wechat_price');
-        $this->addColumn('{{%balance}}', 'watsapp',$this->string(20));
+        //$this->addColumn('{{%balance}}', 'watsapp',$this->string(20));
         return false;
     }
 }
