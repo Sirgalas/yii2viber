2442f7ff367f37d0f81c827338adf44c5f45dc26
sirgalas <hudos@rambler.ru>
2018-06-11 22:46:53 +0300

    redact exel

--- frontend/views/statistics/_mongo.php
+++ frontend/views/statistics/_mongo.php
@@ -1,34 +1,105 @@
 <?php
 use kartik\grid\GridView;
 use frontend\widgets\StatisticGraphMongo;
+use kartik\export\ExportMenu;
 /**
  * @var $searchModel common\entities\mongo\Message_Phone_List;
  * @var $dataProvider yii\data\ActiveDataProvider
  * @var $model common\entities\mongo\Message_Phone_List;
  * @var $providerFromGetModel yii\data\ActiveDataProvider
  */
-$defaultExportConfig = [
-    GridView::EXCEL => [
-        'dataProvider'=>$exeportExelDataProvider,
-        'label' => Yii::t('kvgrid', 'Excel'),
-        'icon' =>'file-excel-o',
-        'iconOptions' => ['class' => 'text-success'],
-        'showHeader' => true,
-        'showPageSummary' => true,
-        'showFooter' => true,
-        'showCaption' => true,
-        'filename' => Yii::t('kvgrid', 'grid-export'),
-        'alertMsg' => Yii::t('kvgrid', 'The EXCEL export file will be generated for download.'),
-        'options' => ['title' => Yii::t('kvgrid', 'Microsoft Excel 95+')],
-        'mime' => 'application/vnd.ms-excel',
-        'config' => [
-            'worksheet' => Yii::t('kvgrid', 'ExportWorksheet'),
-            'cssFile' => ''
-        ]
-    ],
-]; ?>
-<?= StatisticGraphMongo::widget(['model'=>$providerFromGetModel->getModels()]) ?>
 
+$gridColumns = [
+    [
+        'attribute'=>'created_at',
+        'header'=>'Дата рассылки',
+        'value'=> function($model){
+            return date('d:m:Y',$model->viberTransaction->created_at);
+        }
+    ],
+    [
+        'attribute'=>'phone',
+        'header'=>'Phone',
+    ],
+    [
+        'attribute'=>'status',
+        'header'=>'Статус',
+        'value'=>function($model){
+            return $model->statusMessage;
+        }
+    ],
+    [
+        'attribute'=>'date_delivered',
+        'header'=>'Дата доставки',
+        'value'=> function($model){
+            return ($model->date_delivered)?date('d:m:Y',$model->date_delivered):'не доставлено';
+        }
+    ],
+    [
+        'attribute'=>'date_viewed',
+        'header'=>'Дата просмотра',
+        'value'=> function($model){
+            return ($model->date_viewed)?date('d:m:Y',$model->date_viewed):'не просмотрено';
+        }
+    ],
+    [
+        'attribute'=>'viber_message_id',
+        'header'=>'Название рассылки',
+        'format'=>'raw',
+        'value'=>function($model){
+            return \yii\helpers\Html::a($model->viberMessage->title,\yii\helpers\Url::to(['/viber-message/update','id'=>$model->viberMessage->id]));
+        }
+    ],
+];?>
+
+
+<?= StatisticGraphMongo::widget(['model'=>$providerFromGetModel->getModels()]) ?>
+<?php $export=ExportMenu::widget([
+    'dataProvider' => $exeportExelDataProvider,
+    'enableFormatter'=>false,
+    'columns' => $gridColumns,
+    'target' => ExportMenu::TARGET_BLANK,
+    'fontAwesome' => true,
+    'pjaxContainerId' => 'kv-pjax-container',
+    'showColumnSelector'=>false,
+    'dropdownOptions' => [
+        'label'=>'Скачать отчет',
+        'fontAwesome' => true,
+        'class'=> "btn btn-primary",
+        'itemsBefore' => [
+            '<li class="dropdown-header">Получить все данные</li>',
+        ],
+    ],
+    'exportConfig'=>[
+        ExportMenu::FORMAT_TEXT => false,
+        ExportMenu::FORMAT_PDF => false,
+        ExportMenu::FORMAT_HTML=> false,
+        //ExportMenu::FORMAT_CSV=>false,
+        ExportMenu::FORMAT_EXCEL => [
+            'label' => Yii::t('kvexport', 'Excel 95 +'),
+            /*'icon' => $isFa ? 'file-excel-o' : 'floppy-remove',
+            'iconOptions' => ['class' => 'text-success'],
+            'linkOptions' => [],
+            'options' => ['title' => Yii::t('kvexport', 'Microsoft Excel 95+ (xls)')],
+            'alertMsg' => Yii::t('kvexport', 'The EXCEL 95+ (xls) export file will be generated for download.'),
+            'mime' => 'application/vnd.ms-excel',
+            'extension' => 'xls',
+            'encoding'=>'utf-8',
+            'writer' => ExportMenu::FORMAT_EXCEL*/
+        ],
+        ExportMenu::FORMAT_EXCEL_X => [
+            'label' => Yii::t('kvexport', 'Excel 2007+'),
+            'icon' => $isFa ? 'file-excel-o' : 'floppy-remove',
+            'iconOptions' => ['class' => 'text-success'],
+            'linkOptions' => [],
+            'options' => ['title' => Yii::t('kvexport', 'Microsoft Excel 2007+ (xlsx)')],
+            'alertMsg' => Yii::t('kvexport', 'The EXCEL 2007+ (xlsx) export file will be generated for download.'),
+            'mime' => 'application/application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            'extension' => 'xlsx',
+            'writer' => ExportMenu::FORMAT_EXCEL_X
+        ],
+    ]
+]); ?>
 <?=GridView::widget([
     'dataProvider' => $dataProvider,
     'filterModel' => $searchModel,
@@ -37,46 +108,7 @@ $defaultExportConfig = [
     'filterRowOptions' => ['class' => 'kartik-sheet-style'],
     'columns' => [
         ['class' => 'yii\grid\SerialColumn'],
-        [
-            'attribute'=>'created_at',
-            'header'=>'Дата рассылки',
-            'value'=> function($model){
-                return date('d:m:Y',$model->viberTransaction->created_at);
-            }
-        ],
-        [
-            'attribute'=>'phone',
-            'header'=>'Телефон',
-        ],
-        [
-            'attribute'=>'status',
-            'header'=>'Статус',
-            'value'=>function($model){
-                return $model->statusMessage;
-            }
-        ],
-        [
-            'attribute'=>'date_delivered',
-            'header'=>'Дата доставки',
-            'value'=> function($model){
-                return ($model->date_delivered)?date('d:m:Y',$model->date_delivered):'не доставлено';
-            }
-        ],
-        [
-            'attribute'=>'date_viewed',
-            'header'=>'Дата просмотра',
-            'value'=> function($model){
-                return ($model->date_viewed)?date('d:m:Y',$model->date_viewed):'не просмотрено';
-            }
-        ],
-        [
-            'attribute'=>'viber_message_id',
-            'header'=>'Название рассылки',
-            'format'=>'raw',
-            'value'=>function($model){
-                return \yii\helpers\Html::a($model->viberMessage->title,\yii\helpers\Url::to(['/viber-message/update','id'=>$model->viberMessage->id]));
-            }
-        ],
+        $gridColumns[0],
     ],
     'bordered'=>false,
     'striped'=>false,
@@ -88,10 +120,10 @@ $defaultExportConfig = [
         'type' => GridView::TYPE_PRIMARY,
         'heading' => false,
     ],
-    'toolbar' =>  [
-        '{export}',
+    'toolbar' => [
+            $export
     ],
-    'export' => [
+    /*'export' => [
         'label'=>'Скачать отчет',
         'fontAwesome' => true,
         'options'=>[
@@ -99,9 +131,9 @@ $defaultExportConfig = [
         ],
     ],
     'exportContainer' => ['class' => 'btn-primary'],
-    'persistResize' => false,
+    //'persistResize' => false,
     'toggleDataOptions' => ['minCount' => 10],
     'exportConfig' => $defaultExportConfig,
     'itemLabelSingle' => 'book',
-    'itemLabelPlural' => 'books'
+    'itemLabelPlural' => 'books'*/
 ]);?>
